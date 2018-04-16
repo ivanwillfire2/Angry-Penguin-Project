7d39d1cce03f510a5519fc2d43da4b2bbb86d9d4
Ivan Anyaegbu <ioa13a@lib-301-22.acu.local>
2018-04-15 23:25:33 -0500

    Catapult added

--- Angry Penguin Project/Angry Penguins/Angry Penguins/GameScene.swift
+++ Angry Penguin Project/Angry Penguins/Angry Penguins/GameScene.swift
@@ -1,26 +1,26 @@
-//
-//  GameScene.swift
-//  Angry Penguins
-//
-//  Created by Ivan Anyaegbu on 4/15/18.
-//  Copyright © 2018 Ivan Anyaegbu. All rights reserved.
-//
-
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    /* Game object connections */
+    var catapultArm: SKSpriteNode!
     
     override func didMove(to view: SKView) {
+        /* Set reference to catapultArm SKSpriteNode */
+        catapultArm = childNode(withName: "catapultArm") as! SKSpriteNode
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        /* Add a new penguin to the scene */
+        let penguin = MSReferenceNode(fileNamed: "Penguin")
+        addChild(penguin)
+        
+        /* Move penguin to the catapult bucket area */
+        penguin.avatar.position = catapultArm.position + CGPoint(x: 32, y: 50)
     }
     
     override func update(_ currentTime: TimeInterval) {
-
+        
     }
 }

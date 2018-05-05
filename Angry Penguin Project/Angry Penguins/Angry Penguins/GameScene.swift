aa9043916bd3f5288401c3e5d035b2212c9563df
Ivan Anyaegbu <ioa13a@lib-301-19.acu.local>
2018-05-05 18:01:13 -0500

    Added Level and Camera control

--- Angry Penguin Project/Angry Penguins/Angry Penguins/GameScene.swift
+++ Angry Penguin Project/Angry Penguins/Angry Penguins/GameScene.swift
@@ -4,10 +4,33 @@ class GameScene: SKScene {
     
     /* Game object connections */
     var catapultArm: SKSpriteNode!
+    /* Level loader holder */
+    var levelHolder: SKNode!
+    
+    /* Tracking helpers */
+    var trackerNode: SKNode? {
+        didSet {
+            if let trackerNode = trackerNode {
+                /* Set tracker */
+                lastTrackerPosition = trackerNode.position
+            }
+        }
+    }
+    var lastTrackerPosition = CGPoint(x: 0, y: 0)
+    var lastTimeInterval:TimeInterval = 0
     
     override func didMove(to view: SKView) {
         /* Set reference to catapultArm SKSpriteNode */
         catapultArm = childNode(withName: "catapultArm") as! SKSpriteNode!
+        
+        /* Set reference to levelHolder SKNode */
+        levelHolder = childNode(withName: "levelHolder")
+        
+        /* Load Level 1 */
+        let resourcePath = Bundle.main.path(forResource: "Level1", ofType: "sks")
+        let level = SKReferenceNode (url: URL (fileURLWithPath: resourcePath!))
+        levelHolder.addChild(level)
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -30,9 +53,33 @@ class GameScene: SKScene {
         
         /* Apply impulse to penguin */
         penguin.avatar.physicsBody?.applyImpulse(force)
+        
+        /* Set tracker to follow penguin */
+        trackerNode = penguin.avatar
     }
     
     override func update(_ currentTime: TimeInterval) {
         
+        /* Check there is a node to track and camera is present */
+        if let trackerNode = trackerNode, let camera = camera {
+            
+            /* Calculate horizontal distance to move */
+            let moveDistance = trackerNode.position.x - lastTrackerPosition.x
+            
+            /* Duration is time between updates */
+            let moveDuration = currentTime - lastTimeInterval
+            
+            /* Create a move action for the camera */
+            let moveCamera = SKAction.moveBy(x: moveDistance, y: 0, duration: moveDuration)
+            camera.run(moveCamera)
+            
+            /* Store last tracker position */
+            lastTrackerPosition = trackerNode.position
+        }
+        
+        /* Store current update step time */
+        lastTimeInterval = currentTime
     }
 }
+
+
